e64bd2b3cfacf0fa42cede7bcd8617b8021d2ca2
AlfaPapa <papa@alfa.com>
2018-12-19 12:31:39 +0100

    fix file log in element

--- agbot/element/core.py
+++ agbot/element/core.py
@@ -18,16 +18,11 @@ from agbot.session import Session, parseApiError
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler('logs/element%s.log' % (time.strftime("%Y%m%d")))
 c_handler.setLevel(logging.DEBUG)
-f_handler.setLevel(logging.WARNING)
 # Create formatters and add it to handlers
 c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
 logger.addHandler(c_handler)
-logger.addHandler(f_handler)
 
 class Element(object):
     """
