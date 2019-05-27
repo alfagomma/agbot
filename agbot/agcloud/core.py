0c92564ca9ebe7ebc43e5ca238d325e855b2480a
AlfaPapa <papa@alfa.com>
2019-05-27 23:35:52 +0200

    start sdk,logger,test api

--- agbot/agcloud/core.py
+++ agbot/agcloud/core.py
@@ -7,8 +7,8 @@ AGCLOUD SDK
 """
 
 __author__ = "Davide Pellegrino"
-__version__ = "1.1.3"
-__date__ = "2018-10-19"
+__version__ = "1.1.2"
+__date__ = "2019-050-27"
 
 import json
 import time
@@ -16,6 +16,13 @@ import logging
 from agbot.session import Session, parseApiError
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.WARNING)
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+logger.addHandler(c_handler)
 
 class AGCloud(object):
     """
@@ -29,4 +36,17 @@ class AGCloud(object):
         logger.debug('Init AGCloud')
         session = Session()
         self.apibot = session.apibot
-        self.ep_element = session.ep_element
+        self.ep_agcloud = session.ep_agcloud
+
+    #erp
+    def getErp(self, erp_id:int, params=None):
+        """
+        Get ERP data.
+        """
+        logger.debug(f'Get erp {erp_id}')
+        rq = f'{self.ep_agcloud}/erp/{erp_id}'
+        r = self.apibot.get(rq, params=params)
+        if 200 != r.status_code:
+            return False
+        erp = json.loads(r.text)
+        return erp
