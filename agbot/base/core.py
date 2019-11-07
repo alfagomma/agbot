1a6c7eee185e436a5e9b54482049099d9e5f1e38
nuc <nuc@milano1.it>
2019-11-07 17:05:08 +0100

    new base module

---
+++ agbot/base/core.py
@@ -0,0 +1,85 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+BASE SDK
+"""
+
+__author__ = "Davide Pellegrino"
+__version__ = "2.1.1"
+__date__ = "2019-11-07"
+
+import json, logging
+from agbot.session import Session, parseApiError
+
+logger = logging.getLogger()
+
+class Base(object):
+    """
+    AGCloud BASE Data core class .
+    """
+    endpoint = None
+
+    def __init__(self, profile_name='default'):
+        """
+        Initialize main class with this and that.
+        """
+        logger.debug('Init Base SDK')
+        session = Session(profile_name)
+        self.apibot = session.apibot
+        self.endpoint = f'{session.ep_agapi}'
+
+    #erp
+    def getErp(self, erp_id:int, params=None):
+        """
+        Get ERP data.
+        """
+        logger.debug(f'Get erp {erp_id}')
+        rq = f'{self.endpoint}/erp/{erp_id}'
+        r = self.apibot.get(rq, params=params)
+        if 200 != r.status_code:
+            return False
+        erp = json.loads(r.text)
+        return erp
+
+    #unit of measure
+    def getUoms(self, query=None):
+        """Get all uoms."""
+        logger.debug('Getting all unit of measure...')
+        rq = f'{self.endpoint}/unitofmeasure'
+        r = self.apibot.get(rq, params=query)
+        if 200 != r.status_code:
+            return False
+        uoms = json.loads(r.text)
+        return uoms
+
+    def getUom(self, uom_id:int, query=None):
+        """
+        Leggo uom da id.
+        """
+        logger.debug(f'Reading family {uom_id}...')
+        rq = f'{self.endpoint}/unitofmeasure/{uom_id}'
+        r = self.apibot.get(rq, params=query)
+        if 200 != r.status_code:
+            return False
+        uom = json.loads(r.text)
+        return uom
+
+    def getUomFromCode(self, code:str, query=None):
+        """
+        Leggo uom da code.
+        """
+        logger.debug(f'Reading uom code {code}...')
+        params = {
+            'code': code
+            }
+        if query:
+            new_params = dict(item.split("=") for item in query.split('&'))
+            params = {**params, **new_params}     
+        rq = f'{self.endpoint}/unitofmeasure/findByCode'
+        r = self.apibot.get(rq, params=params)
+        if 200 != r.status_code:
+            return False
+        uom = json.loads(r.text)
+        return uom        
