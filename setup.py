f394e00744e094c2431c949bc0be99792e917f4f
nuc <nuc@milano1.it>
2019-11-08 15:40:37 +0100

    wip setup

--- setup.py
+++ setup.py
@@ -11,11 +11,6 @@ from setuptools import setup, find_packages
 ROOT = os.path.dirname(__file__)
 VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
 
-requires = [
-    'redis>=3.0',
-    'requests>=2.7',
-]
-
 def get_version():
     init = open(os.path.join(ROOT, 'agbot', '__init__.py')).read()
     return VERSION_RE.search(init).group(1)
